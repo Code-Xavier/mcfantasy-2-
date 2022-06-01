22d99550a463120b7c7d921bfbe4837ecebd356c
Xavier Valois-Smith <96591678+Code-Xavier@users.noreply.github.com>
2022-05-31 20:53:21 -0700

    "rendering chapters with JS DOM manipulation now"

---
+++ pages/sotc/ch0.js
@@ -0,0 +1,47 @@
+let chapter0 = document.getElementById('chapter0')
+
+//CHAPTER 0 (INTRODUCTION)
+
+chapter0.innerText = `
+
+
+Hi.
+
+My name is Asher Hexun.
+
+This account you’re reading is my origin story. 
+
+You’ll be saying a whole lot of “what the hell!?” and “that’s impossible!” as you go through this account of my adventures as a young Dra - wait. I can’t tell you that yet. 
+
+Anyway, suffice it to say that the story I’m about to tell you will seem straight up out of a fantasy novel, no shit. I’ll tell it exactly as it happened, and allow you to draw your own conclusions as to the validity and authenticity of my account. 
+
+My story begins on a cold windy night in the middle of October.
+
+Most kids are born in hospitals with the help of nurses and doctors. Some fathers hold a camera filming the moment of their birth; other fathers hold the mother’s hand, providing mental and moral support, encouraging her in performing the miracle of childbirth.
+
+The baby is born, and the doctor holds up the little bundle for their awaiting parent to see. Their eyes filling with tears of joy and happiness as they finally meet their beloved child, the one they waited nine months to meet; and once the doctor hands the baby over to them, they cuddle it and tell him or her just how much they love and treasure them. 
+
+But as you probably guessed, none of this was the case for me. Like everything else that followed in my life, my entrance into this world wasn’t exactly normal.
+
+The wind rattled against the small windows of a remote cottage in the middle of an unnamed forest as my mother pushed me out, her screams drowning out the noise outside as my father stood off to the side observing her helplessly, as she had refused any assistance from him.
+
+With one final cry of agony, I came out onto the cold bed, her blood staining the white sheets a deep shade of crimson. “It’s done” she snarled at my father as she violently cut the umbilical cord and weakly got up, not even looking at her child as she dizzily stumbled to the bathroom. 
+
+Still keeping an eye on her, my father approached the bed and gently picked me up, cradling me in his arms and softly swaying me side to side as my cries pierced the air and ricocheted off the walls, their sound filling the entire room.
+
+“Good job Matilda,” he said, turning towards my mother. “Your part of the deal is fulfilled - you may leave”. She scuffed, giving him one final poisonous glare as she closed the bathroom door.
+
+Father gingerly cleaned and dressed me, checking me over to make sure I was healthy and comfortable. He remarked to himself that he would have to make me a bottle of formula milk later since it would be too much of a stretch to ask my mother to breastfeed me at least once.
+
+When my mom came out of the bathroom half an hour later, my father was still waiting in the room to make sure she was okay. Her dark-brown eyes stared at the baby in his arms with undisguised disgust; Then she turned around, grabbed her stuff, and opened the door as a gust of wind entered the room with a vengeance, prowling the room like a hungry animal, turning everything this way and that, pushing against her as she fought her way outside, and slammed the door behind her with a bang.
+
+That was the last I ever saw of my mother. Not that I remember any of this at all, as I learned it from my father word for word as I have told it to you. It may seem rash to narrate it in such a way, but he is a man of few words and not prone to sugar coat things, however painful. It was definitely painful to my then seven-year-old self the first time he told me what had happened to my mother.
+
+Unfortunately, life didn’t get rid of the weirdness that marked my birth. My earliest memories were of moving around from state to state and country to country, always on the move and yet never arriving at our destination. Sometimes strange people would come to visit our house and sit around the living room talking with my dad. On such occasions, I would be placed in the middle of the room and given various toys,which I would be encouraged to play with while my father and the other people observed me closely, taking notes.
+
+ After some time had passed, they would speak a while longer with my father, and then leave. These encounters were such a normal part of my life that I was actually confused when they stopped coming when I turned four, never to be mentioned again by my father. I never really got around to asking, and sometimes I still wonder if I had imagined the whole thing. It doesn’t help that no matter how hard I try I couldn't remember their faces.
+
+It had been a long time since I even bothered to think about that deeply buried memory, its obscure presence only coming to mind as it was forcefully pulled from my subconsciousness on the morning of my fifteenth birthday as I found myself face to face with one of these mysterious individuals in a quiet dark room. That’s when everything changed… and not for the better.
+
+
+`
---
+++ pages/sotc/ch5.js
@@ -0,0 +1,123 @@
+let chapter5 = document.getElementById('chapter5')
+
+
+chapter5.innerText = `
+
+
+“Somehow, I survived my first week of school.” I thought as I sat outside with Daenerys and a few of the other theatre kids. Practice had just finished and a lot of them were still waiting to be picked up. Dad told me He would be a few minutes late so that I wouldn't worry about him being late. Sara had left almost as soon as practice was finished, a white jeep wrangler had picked her up. At least she waved to me before leaving.
+
+Despite my best efforts to be distrustful of Sara I just couldn't muster the energy for it, which was unfair since I was so quick to be weary of Daenerys despite her being a lot friendlier towards me. I turned to look at her, sitting on the concrete steps in front of the school just a foot away from me. “Hey Dan?”, I began, using her nickname “Can I ask you something?”.
+
+She looked up from her phone where she had been shuffling through music, one earbud on. “Sure Asher '', she said in her usual cheery tone, “What up?” “Um… this may come out sounding wrong”, I began, fidgeting nervously, “But why… why did you befriend me? that first day in school why did you help me out?... I mean thank you so much for it but...why?”. It had been a question that had plagued me from the moment I receive that strange warning, why had Daenerys been so willing to help me? She could have just passed by me that day in the hallway, or just showed me my first classroom but she had gone out of her way to guide me around the school, which I found strange, or maybe she was a much nicer person than I.
+
+Her expression turned more serious, almost sad, “You… well you just remind me of him”. She said, looking away from me and to somewhere far away.’’ “My older brother I mean…”, she added, “He passed away two years ago… car accident, my mom was driving him to the mall and well...  an eighteen-wheeler ran into them, neither survived”. I stood perfectly still in stunned silence, unable to come up with an even close to an appropriate response to what she had just said.
+
+She smiled sadly, “He was a lot like you, really quiet and shy, I remember he would cry every first day of primary school, despite me being two years younger than him I would always console him and help him find his classroom… when I saw you that day in the hallway with that terrified expression you just reminded me so much of him… It just felt like I was helping him when I helped you…”, she said, looking back at me, “I'm sorry if you think that's weird”.
+
+I shook my head vigorously, “No, no it's fine... .I’m really sorry for what happened”, I said. “It's alright really… I'm really grateful for your help… and your friendship, I don't mind at all”. She smiled again but this time I could see that she had taken off a mask, leaving herself open for me to see just the hurt and pain she hid under her humor and laughter. It reminded me of Jake and how he also wore a mask, that of a happy-go-lucky boy who didn’t care much about anything but was truly working himself to the bone, chasing a dream that wasn’t even his own and I began to realize something...
+
+I had thought I was the only one hiding who I was, the only one with serious worries but now I began to see that everybody more or less was wearing a mask to hide what they were really going through in private, what pain they bore on the inside and suddenly none of the kids around me looked the same, I wondered what they had suffered, what made them happy, what made them sad,  how they coped with life and everything it threw at them.
+
+“Thank you Asher”, she said softly after a while, “You are a good friend”.
+
+I wasn't sure how to respond to that but I noticed she had scooted closer as we talked, “Um… You want a hug?”, I asked, the only thing I could think to say. She chuckled, “You really are just like him”, she said, leaning in and allowing me to put my arms around her shoulders awkwardly. That was the first time I had hugged anybody apart from my dad, and unlike him, she was really small and her hair smelled of strawberries and ginger, quite a strange combination.
+
+As I held her in my arms all my doubts about her began to fade, it was hard for them not to with the story she had told me, and I believed her, not even because I could monitor her heartbeat to detect lies, because even if I couldn't I would still have believed her just because of the sincerity in her eyes, that genuine hurt; but mainly it was because I realized I trusted her, completely and utterly. “Thank you”, she said, letting go of me after a while as an autumn wind picked up a few stray leaves and blew them across the sidewalk.
+
+I smile softly, “No, thank you”.
+
+
+7:00 PM 
+
+
+I bit my tongue to stop myself from screaming as I slammed against the wall with such strength the concrete shattered, and I felt that my shoulder was dislocated. “Get up”, My dad said, standing at the other end of the basement like an executioner awaiting his victim. Shakily, I tried to get to my feet, using the wall as support, “My shoulder…”, I said in between trying to catch my breath. “Put it back in, you know how to do it”, he answered gruffly. I gritted my teeth, fully aware of how much this was going to hurt as I grabbed my wrist and pulled until the bone popped back into place.
+
+Dad was still waiting for me, standing on his end of the floor. He got into a fighting position as soon as I was done fixing my shoulder. Immediately I followed suit, not wanting to get thrown against the wall like a rag doll again. This time I tried something different, I charged at him instead of waiting for him to make the first move jumping to try and land a kick on his head. He was almost half a foot taller than me, standing at around six feet, he also was heavier with more muscle mass making him harder to move, but at least I was faster.
+
+My heel connected with his jaw, making him stumble back slightly. I tried to use my momentum and weight to push him to the floor but it wasn’t enough, and he had predicted it. He grabbed my foot and slammed me against the hard floor, knocking the air out of me. I saw black spots in my vision as I tried to catch my breath again, but he wasn’t about to let me get the chance. He tried to put me in a chokehold but I had also learned his tricks. Using all my strength, I punched him in the sternum and sent him back a few feet.
+
+Not wasting any time I got back to my feet with a kick up just in time to block a punch, and another and another. He was trying to overwhelm me now with a flurry of punches and kicks barely giving me the chance to block much less counterattack. This was looking bad, I was going to lose again. Usually, I could squish in a couple of victories, and towards the end of the past few months, I had begun to win more and more to the point where it had become 50/50. Today, however, I wasn’t exactly at my fullest, it was Sunday afternoon and the weakening of my abilities was definitely fading, but I wasn’t there quite yet.
+
+
+
+I blocked a high kick that came dangerously close to hitting home and decided that I might as well try something new, see if I could at least win at least once today. The thing with my dad was that he was an expert fighter, and no matter how much I tried to read his movement I couldn't quite predict what he was about to do. All I could go off was the subtle tensing of particular muscles and slight movements and even then it was still far from accurate. 
+
+If this was going to work I had to take a risk. I observed him closely, all the while blocking his attack as we both danced around each other in an impossibly fast dance, one where a single miscalculation would end up with me losing. It took a while but I finally saw my chance, putting all my speed into it I duked, swiping his feet from under him in a movement that was so fast even I was surprised for a second as he went down.
+
+My surprise didn’t last long as I jumped into action, as the way to win was to immobilize him or knock him out. I could try to get him in a hold, but I would have to stun him first, otherwise, I would be the one ending up in a lock. I raised my foot up and brought it down with an axe kick, landing my heel down on the dead center of his face, all the while silently apologizing to him in my head. The sound of bones shattering as my foot hit its mark made me hesitate for a few seconds, a hesitation that probably cost me the fight. Before I knew it I was the one on the ground, dad having grabbed my ankle and, much like I had done to him only moments before, pulled my feet from under me.
+
+My ears rang as my head hit the hard concrete floor, my teeth closing so hard on themselves I tasted blood from chopping off a piece of my tongue and I landed face up. “Don't ever hesitate in a fight”, Dad told me now standing over me, one of his knees on my neck, to cut off my air supply, and the other on my chest using all of his weight to make it harder for me to throw him off. His face was bloodied, his nose was completely ruined and I could see some pieces of cartilage sticking out here and there, I wanted to feel guilty about it but on the other hand, he was very much choking me at the moment.
+ 
+I tried to struggle free, punching him, arching my back trying to unbalance him just the slightest bit but nothing was working and all my struggle was doing was quickly diminish my limited supply of oxygen. Black spots began to dance across my vision as my lungs burned demanding oxygen I couldn't provide, I was close to passing out and had no more strength to fight. Despairing, I tapped his leg three times, the signal of surrender. He freed me, and I took a huge mouth full of air, gulping it down greedily as my dad observed me with a stone-cold face.
+
+“You have two minutes to get yourself together”, he said, walking away from me. Already I could see his face healing but he likely also wanted time to recuperate himself. Both of us were drenched in sweat, our gym clothes covered in dirt and dust from the basement floor as well as bloodstains from all the injuries we had gotten from practicing, how long had it been now? I had completely lost track of time, but it felt like we had been practicing throughout the entire day.
+
+I felt exhausted, my muscles screamed every time I moved and I ached everywhere from injuries that hadn’t gotten a full chance to heal. Dad was pushing me to my very limits, I could sense a new form of urgency in him. Usually, he was calmer, more patient, and not nearly as brutal. From the moment he had picked me up Friday afternoon from school I had felt it, he could no longer hide the tension in his body, the nervous tapping of his fingers. That nervousness had apparently translated into pushing my training to a whole new extreme, beating the living daylights out of me. It was clear he was no longer holding back.
+
+From the moment I could walk dad began training me, and I couldn't even remember what my life had been like before the weekly practice sessions. They had been fun when I was little but gradually they had gotten harder and harder, more violent as dad stopped holding back more and more. I had never questioned why he had trained me to fight, or even what technique he used. From what I could gather it was a mix of a lot of martial arts, just with all of their prohibited techniques thrown in.
+
+Of course with our strength and speed the fights often ended with some significant damage to the property, not to mention being so noisy that our neighbors probably thought we had adopted a pet rhinoceros. Because of this Dad had moved them to the basement years ago, I couldn't remember the last time we had lived in a house without one. From his end of the room dad got up from his perch on a folding chair, “Time up”, he said, getting into a fighting position.
+
+Trying not to throw up I got to my feet unsteadily. As we fought over the last few hours all I could think was all the craziness that had gone on in my life recently, the strange messengers that came to me in my dreams, the weakening of my abilities seemingly from my dad’s own hand, and overall his words as he cried thinking I was sleep, that day I had understood that if I wanted to stay with him there would be a price to pay, at the time I had thought it would only be the lethargy that now plagues my life, now I realized this was also part of it - out here it was dangerous, and I needed to fight to survive.
+
+The anger and lack of control I felt over my own life helped me back to my feet, fueling me to where my body could no longer manage it. It made me hold my fist firmly in a guarding position in preparation for the fight, if this was the real world, if this is what it took to stay with my father, then I would fight to my very last breath.
+
+
+
+9:00 PM
+
+
+
+My nightstand clock marked 9:00 p.m as I crawled into my bed feeling like I had been put through a meat grinder. I couldn't see how I could possibly move tomorrow to go to school, I had so many injuries my healing was having trouble catching up. The only thing that had stopped me from crashing down back in our basement was the hunger that burning so much energy brought, and I had to eat to help my healing factor along. Despite my earlier failures I had managed to get a few victories, it hadn’t been much but I had also been running on fumes and anger so there was that.
+
+I debated whether or not I had the energy to get the covers around me when I saw my phone screen illuminate, letting me know there was a new message. Under normal circumstances, no person’s message, short of the president of the United States, would make me stretch my arm and grab the phone. However, my eyes had managed to catch who the message was from. Instantly I grabbed the phone and with newfound energy opened the message from Sara.
+
+“Hi”, she wrote
+“Are you going to school tomorrow?”.
+
+It took me a few seconds to calm myself down from the sudden excitement that she was messaging me to actually reply.
+
+“Yeah, I am”.
+
+Almost immediately she replied.
+
+“Good”.
+“Because I won't be able to make it to practice so make sure nobody touches my murals”.
+
+A wave of disappointment hit me when I realized I won't be able to see her during practice; maybe I could catch her during school hours? It seemed unlikely since I hadn't seen her in any of my classes.  Shaking my head, I wondered what was wrong with me, I really was turning into an idiot, or more than I had already been, it was like just thinking about her significantly reduced my thinking capabilities.
+
+“Okay, sure no prob”. I wrote back
+
+“I'll keep them safe”.
+
+She sent me several laughing emojis.
+
+
+“Sure you will”, she wrote back and I could practically feel the sarcasm through her words.
+“If you can remember what they look like”.
+
+For the life of me I couldn't figure Sara out -, one moment she was nice, the next she was calling me stupid to my face, even when she was asking me for a favor. I thought of asking my dad to give me a crash course on how to understand women 101, but considering my mom had dumped him with a newborn baby, I wasn’t sure he was a reliable source. Maybe Jake could help me out, but asking him about it would only bring on a torrent of jokes at my expense, not to mention that would confirm to him his notion that something was going on between me and Sara and then he would tell everybody and just publicly humiliate me, nope, definitely no.
+
+“Okay… sorry not sorry for that”. Sara wrote before I could come up with a response to her last text.
+“But seriously, you looked weird Friday, maybe say no to drugs, ever heard of that?”
+
+There she went again, I thought, looking at my phone like some kids looked at their calculus books, with complete and utter confusion, no hope shining in their eyes of ever understanding.
+
+“Anyway…”, she once again wrote before I could reply, she had some crazy texting speed.
+“Maybe I'll see you tomorrow, idk, but keep my murals safe”.
+
+Finally, it seemed she had run out of steam giving me a chance to reply.
+
+“Okay, don't worry”.
+
+In the second she had once again replied.
+
+“I would thank you but I'll save that for later when I find my murals safe”.
+
+With that, she disconnected, no goodbyes of any sort. I held the phone for a few minutes, hoping she would again write something but she remained disconnected and finally, my tiredness won over.  I put the phone back on my night table and laid back on the bed. I was asleep before my head could hit the pillow.
+
+
+
+
+
+`
